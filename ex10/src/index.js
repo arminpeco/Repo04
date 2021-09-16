4d2c3d918001f6e9249a1f4e9d12cfc01876dcb7
arminpeco <apeco1906@gmail.com>
2021-09-16 23:52:43 +0200

    Added file index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne,numTwo){
+    // only change code below this line
+    var myFirstName="Armin";
+    var myLastName="Peco";
+    var myHomeTown="Mostar";
+    // only change code above this line 
+
+
+return {
+    myFirstName,
+    myLastName,
+    myHomeTown
+};
+}
+
+console.log(main());
+module.exports=main;
